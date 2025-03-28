1ba6d578e41e7f1ce7f06e0d015f513f4f1516f4
Bradlee Harrison <bradlee.harrison22@gmail.com>
2025-03-27 19:10:24 -0500

    Added list of areas and added ability to crop the image by a scaling factor to save on processing time. Current code is still very slow and would likely not work without significant improvements.
    Also have been limited by template matching. Template matching is very sensitive to shifts in scale or rotations, two things that will be extremely variable in this. Solution may be to use ORB instead of template matching, but works as a basic proof of concept.

--- src/video-grabber.py
+++ src/video-grabber.py
@@ -4,89 +4,108 @@ import numpy as np
 import pyaudio
 import time
 
+
 import vlc
 
 
 
 from pywinauto import Desktop, Application
 
+# Crop images to save processing power
+def crop_image(game_image, crop_fraction=0.30, preview=False):
 
 
-app = Application(backend="uia").connect(title="RotMGExalt")
-app.RotMGExalt.set_focus()
+    height, width = game_image.shape[:2]
+    ch = int(height * crop_fraction)
+    cw = int(width * crop_fraction)
+    y1 = (height - ch) // 2
+    x1 = (width - cw) // 2
 
-window = app.window(title="RotMGExalt")
+    if preview:
+        preview = game_image.copy()
+        cv2.rectangle(preview, (x1, y1), (x1 + cw, y1 + ch), (0, 255, 0), 2)
+        cv2.imshow("Crop Region", preview)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
-tracks = {
-    'nexus' : '12_Hadenfeer.flac',
-    'realm-novice' : '02_Odyssey.flac',
-    'sprite' : '15_Arc_Sancts.flac',
-    'deep-sea-abyss' : '11_Mechanical_Ambivalence.flac'
-}
-player = vlc.MediaPlayer()
 
-location = None
-last_location = None
-
-while True:
-    game_image = window.capture_as_image()
+    return game_image[y1:y1+ch, x1:x1+ch]
     
-    for loc in tracks.keys():
+
+
+class Area:
+    def __init__(self, track: str, tolerance: float):
+        self.track = track
+        self.tolerance = tolerance
+
+def main():
+    app = Application(backend="uia").connect(title="RotMGExalt")
+    app.RotMGExalt.set_focus()
+    window = app.window(title="RotMGExalt")
+    areas = {
+        'nexus' :          Area('12_Hadenfeer.flac', 0.5),
+        'realm-novice' :   Area('02_Odyssey.flac', 0.5),
+        'sprite' :         Area('15_Arc_Sancts.flac', 0.5),
+        'deep-sea-abyss' : Area('11_Mechanical_Ambivalence.flac', 0.5),
+    }
+    player = vlc.MediaPlayer()
+
+    location = None
+    last_location = None
+
+    # Load all images into memory. This saves time to do it beforehand rather than repeatedly open the image file. 
+    target_images = {}
+    for loc in areas.keys():
         dir = f'images/ref/{loc}/'
         for ref in os.listdir(dir):
-            try:
-                # Try with opencv template matching
-                game_image = cv2.cvtColor(np.array(game_image), cv2.COLOR_RGB2BGR) 
-                target_image = cv2.imread(dir + ref)
-                result = cv2.matchTemplate(game_image, target_image, cv2.TM_CCOEFF_NORMED)
-                _, max_val, _, max_loc = cv2.minMaxLoc(result)
+            if dir not in target_images.keys():
+                target_images[loc] = {}
+            target_images[loc][ref] = cv2.imread(os.path.join(dir, ref))
 
-                threshold = 0.6
-                
-                print(f"[DEBUG] {ref}: {max_val}")
 
-                if max_val >= threshold:
-                    x, y =  max_loc
-                    location = loc
-                    print(f"[DEBUG] {ref} Match found at ({x}, {y})")
+    for i in range(6000):
+        game_image = window.capture_as_image()
+        game_image = cv2.cvtColor(np.array(game_image), cv2.COLOR_RGB2BGR) 
+        game_image = crop_image(game_image, 0.4, preview=False)
+
+        for loc in target_images.keys():
+            for ref in target_images[loc].keys():
+                try:
+                    # Try with opencv template matching
+                    target_image = target_images[loc][ref]
+                    result = cv2.matchTemplate(game_image, target_image, cv2.TM_CCOEFF_NORMED)
+                    _, max_val, _, max_loc = cv2.minMaxLoc(result)
+
+                    threshold = 0.6
+                    
+                    if max_val > threshold * 0.2:
+                        print(f"[DEBUG] {ref}: {max_val}")
+
+                    if max_val >= areas[loc].tolerance:
+                        x, y =  max_loc
+                        location = loc
+                        print(f"[DEBUG] {ref} Match found at ({x}, {y})")
+                except Exception as e:
+                    print(f"[ERROR] {e}")
+                    print(game_image.shape)
+                    print(target_image.shape)
+
+                if location:
+                    break
+
             
-            except Exception as e:
-                print(f"[ERROR] {e}")
-    
+
+        
         if location != last_location: 
             last_location = location
-            track = f"tracks/{tracks[location]}"
+            track = f"tracks/{areas[location].track}"
             print(f"Track: {track}")
-
-            
             media = vlc.Media(track)
-
-            
             player.set_media(media)
             player.play()
 
-        if not location:
-            print(location)
-            player.stop()
+            if not location:
+                player.stop()
 
-
-
-
-
-
-#screenWidth, screenHeight = pyautogui.size()
-
-
-
-#for i in range(50):
-    #im1 = pyautogui.screenshot('test.png') # Not fast but not slow, may take ~100 ms
-
-    
-    #try:
-        #location = pyautogui.locateOnScreen('images/ref/nexus.png', confidence=0.5)
-        #print(location)
-        #dungeon = ''
-        #print(f"Dungeon {dungeon}")
-    #except Exception as e:
-     #   print(e)
-    
+if __name__ == '__main__':
+    main()
