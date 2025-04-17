a3f796d712b532e89d83a6de071cc741f625db60
Bradlee Harrison <bradlee.harrison22@gmail.com>
2025-04-17 11:15:14 -0700

    Working to add tests.

---
+++ tests/test_crossfader.py
@@ -0,0 +1,26 @@
+from crossfader import Crossfader
+from areas import Areas
+
+from unittest.mock import Mock
+
+def test_init():
+    mock_areas = Mock()
+    mock_areas.dict = {}
+    
+    crossfader = Crossfader(mock_areas.dict)
+
+def test_replay():
+    mock_areas = Mock()
+    mock_areas.dict = {}
+
+    crossfader = Crossfader(mock_areas.dict)
+    crossfader.replay()
+
+def test_crossfade_empty():
+    mock_areas = Mock()
+    mock_areas.dict = {}
+
+    crossfader = Crossfader(mock_areas.dict)
+    crossfader.replay()
+    crossfader.crossfade("")
+    crossfader.crossfade("")
