7abfb2741d52b79443c8ae875323414295b834d4
Bradlee Harrison <bradlee.harrison22@gmail.com>
2025-04-22 22:38:18 -0500

    Updating tests progress 2

--- tests/test_utility.py
+++ tests/test_utility.py
@@ -1,10 +1,15 @@
 import cv2
 import os
+
+from unittest.mock import Mock
+
 from globals import REF_DIR
 
-from utility import find_audio_files,  try_process_sound, crop_image, create_results_empty
+from utility import find_audio_files,  try_process_sound, crop_image, create_results_empty, load_images_and_features
  
+
 test_loc = "nexus/nexus"
+test_area = {test_loc : None}
 
 dir_path   = os.path.join(os.curdir, REF_DIR, test_loc)
 image_path = os.path.join(dir_path, "image.png")
@@ -24,3 +29,20 @@ def test_try_process_sound():
 def test_crop_image():
     img = cv2.imread(image_path)
     crop_image(img)
+    crop_image(img, preview=True)
+
+def test_create_results_empty():
+    mock_areas = Mock()
+    mock_areas.dict = test_area
+    results = create_results_empty(mock_areas.dict)
+    print(results)
+
+def test_load_images_and_features():
+    mock_orb = Mock()
+    mock_areas = Mock()
+    mock_areas.dict = test_area
+    load_images_and_features(dir_path, mock_orb, mock_areas.dict)
+
+if __name__ == '__main__': 
+    test_create_results_empty()
+    
