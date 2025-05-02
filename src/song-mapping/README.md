1f89bce2f477da3fb802a84920f5a48caa9b8137
Bradlee Harrison <bradlee.harrison22@gmail.com>
2025-05-01 22:05:02 -0500

    Add progress towards using a yaml script to generate ref/ folder

--- src/song-mapping/README.md
+++ src/song-mapping/README.md
@@ -1,3 +1,4 @@
 This directory contains a standard mapping to create a ref/ folder
 
 * config.yaml - can be used to set custom music paths
+* generate_ref.py - run this to generate a ref/ folder with music based on the 'config.yaml' file.
---
+++ src/song-mapping/generate_ref.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+import os
+
+
+import yaml
+
+def no_children():
+    
+
+def generate_ref():
+    root = Path("ref/")
+    if root.exists():
+        print(f"Reference directory {root.absolute()} already exists! Please delete this directory and rerun to regenerate songs.")
+        return
+    
+    os.mkdir(root)
+    with open("./config.yaml", "r") as f:
+        y = yaml.load(f, Loader=yaml.FullLoader)
+
+
+        print(y.keys())
+
+        for k in y.keys():
+
+            print(y[k])
+
+
+
+            os.mkdir(Path(root, k))
+
+        
+if __name__ == '__main__': 
+    generate_ref()
